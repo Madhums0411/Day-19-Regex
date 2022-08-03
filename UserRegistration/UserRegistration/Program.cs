daebbad182d00d4872b8aaa718364355c3989804
madhums0411 <madhums41175@gmail.com>
2022-08-03 13:56:23 +0530

    Enter Valid First Name

--- UserRegistration/UserRegistration/Program.cs
+++ UserRegistration/UserRegistration/Program.cs
@@ -1,1 +1,16 @@
-﻿Console.WriteLine("Welcome to User Registration Problem");
+﻿using UserRegistration;
+
+Console.WriteLine("Welcome to User Registration Problem");
+
+RegexFirstName firstName = new RegexFirstName();
+
+if(firstName.validateString("Madhu"))
+
+{
+    Console.WriteLine("The Name is Valid");
+
+}
+else
+{
+    Console.WriteLine("The Name is InValid");
+}
